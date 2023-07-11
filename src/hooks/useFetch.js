1ae078c9b6bfa5b4c562b77999df3630647b8419
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-07-11 19:26:56 -0300

    added api request function

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+const useFetch = () => {
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const url = 'http://localhost:1337/api/blogs'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      try {
+        const res = await fetch(url)
+
+        const json = await res.json()
+
+        setData(json)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [url])
+
+  return {loading, error, data}
+}
+
+export default useFetch
