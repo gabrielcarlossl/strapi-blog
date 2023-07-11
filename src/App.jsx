9ff5b10eda7855d22078a1f078a6e00ccdd26cf9
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-07-11 12:45:58 -0300

    added footer and pagination buttonn style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Blogs from './components/Blogs'
+import Footer from './components/Footer'
 import NavBar from './components/NavBar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <NavBar />
       <Blogs />
+      <Footer />
     </>
   )
 }
